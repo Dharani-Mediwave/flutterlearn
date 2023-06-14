289ccf773f32d6fb431856afc2974b5dfa25ae27
Dharani rajendiran <dharani@mindwaveventures.com>
2023-06-14 15:33:36 +0530

    Learn Material, scafford, TextSpan widget

---
+++ lib/screens/Dashboard.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text.rich(
+        TextSpan(
+          text: 'My',
+          style: TextStyle(
+            fontSize: 30.0,
+          ),
+          children: [
+            TextSpan(
+              text: 'Flutter',
+              style: TextStyle(
+                fontSize: 50.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            TextSpan(
+              text: 'App',
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.w400,
+                color: Colors.blue,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
